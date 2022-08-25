484f71048c7c1f19563044921a30e31efdb198e3
kimziou77 <kimziou77@naver.com>
2022-08-25 16:23:58 +0900

    feat: hello api

---
+++ src/main/java/com/kimyo/cicd/api/helloApi.java
@@ -0,0 +1,14 @@
+package com.kimyo.cicd.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloApi {
+
+    @GetMapping("/")
+    public String healthy(){
+        return "HELLO KIMYO";
+    }
+
+}
